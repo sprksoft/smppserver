4bc4531e95b6325498269b0711f72a1cd8f00692
ldev <ldev@ldev.eu.org>
2024-08-22 12:29:52 +0200

    fix typo in lds_deploy

--- lds_deploy.sh
+++ lds_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-ALLOW_ON_DEPLOY_MAIN="false"
+ALLOW_DEPLOY_ON_MAIN="false"
 ALLOW_UNCOMMITED="false"
 
 year=$(date --utc +'%-Y')
@@ -18,7 +18,7 @@ if [[ "$(git status -s)" != "" ]] && [[ "$ALLOW_UNCOMMITED" == "false" ]] ; then
 fi
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if ! [[ "$BRANCH" != "dev" ]] && [[ "$ALLOW_ON_DEPLOY_MAIN" == "false" ]] ; then
+if ! [[ "$BRANCH" != "dev" ]] && [[ "$ALLOW_DEPLOY_ON_MAIN" == "false" ]] ; then
   echo "Not on dev branch"
   exit 1
 fi
