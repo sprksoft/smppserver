32f12e38d00574deace83ea2b5e1bbd123154fa4
ldev <ldev@ldev.eu.org>
2024-08-30 22:27:11 +0200

    working on only be able to reserver a max of 2 names (no work)

--- smppgc/src/chat/usernamemgr.rs
+++ smppgc/src/chat/usernamemgr.rs
@@ -3,31 +3,44 @@ use std::{
     collections::HashMap,
     fmt::Display,
     ops::Deref,
+    rc::Rc,
     sync::Arc,
     time::{Duration, SystemTime},
 };
 
 use uuid::Uuid;
 struct NameSlot {
+    name: Rc<str>,
     last_used: u64,
     owner: Key,
 }
 impl NameSlot {
-    pub fn new(owner: Key) -> Self {
+    pub fn new(owner: Key, name: Rc<str>) -> Self {
         Self {
-            last_used: Self::current_epoch(),
+            last_used: Self::epoch(SystemTime::now()),
             owner,
+            name,
         }
     }
-    fn current_epoch() -> u64 {
-        SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
+    fn epoch(now: SystemTime) -> u64 {
+        now.duration_since(SystemTime::UNIX_EPOCH)
             .expect("Time went backwards")
             .as_secs()
     }
-    pub fn lease(&mut self, key: Key) {
-        self.last_used = Self::current_epoch();
-        self.owner = key;
+    pub fn lease(&mut self, name: Arc<str>, key: Key, reserve_time: u64) -> Option<NameLease> {
+        if name.as_ref() != self.name.as_ref() {
+            return None;
+        }
+        let now = SystemTime::now();
+
+        let last_used_time = SystemTime::UNIX_EPOCH + Duration::from_secs(self.last_used);
+        let age = now.duration_since(last_used_time).unwrap().as_secs();
+        if self.owner == key || age > reserve_time {
+            self.last_used = Self::epoch(now);
+            self.owner = key;
+            return Some(NameLease(name));
+        }
+        None
     }
 }
 
@@ -45,40 +58,40 @@ impl Into<Arc<str>> for NameLease {
 }
 
 pub struct UsernameManager {
-    names: HashMap<String, NameSlot>,
+    names_to_slot: HashMap<Rc<str>, usize>,
+    leased_slots: HashMap<Key, Vec<usize>>,
+    slots: Vec<NameSlot>,
     reserve_time: u64,
 }
 impl UsernameManager {
     pub fn new(reserve_time: u64) -> Self {
         Self {
             reserve_time,
-            names: HashMap::new(),
+            names_to_slot: HashMap::new(),
+            leased_slots: HashMap::new(),
+            slots: Vec::new(),
         }
     }
-    pub fn lease_name<'a>(&mut self, name: &'a str, key: Key) -> Result<NameLease, NameLeaseError> {
-        if name == "system" {
+
+    pub fn lease_name<'a>(
+        &mut self,
+        name: Arc<str>,
+        key: Key,
+    ) -> Result<NameLease, NameLeaseError> {
+        if name.as_ref() == "system" {
             return Err(NameLeaseError::Taken);
         }
-        let Some(name) = Self::validate_normalize_username(name) else {
+        let Some(name) = Self::validate_normalize_username(&name) else {
             return Err(NameLeaseError::Invalid);
         };
-        if let Some(slot) = self.names.get_mut(name.as_ref()) {
-            let now = SystemTime::now();
-            if slot.owner == key {
-                slot.lease(key);
-                return Ok(NameLease(name.into()));
-            }
-            let last_used_time = SystemTime::UNIX_EPOCH + Duration::from_secs(slot.last_used);
-            let age = now.duration_since(last_used_time).unwrap().as_secs();
-            if age > self.reserve_time {
-                slot.lease(key);
-                return Ok(NameLease(name.into()));
-            }
-        } else {
-            self.names.insert(name.to_string(), NameSlot::new(key));
-            return Ok(NameLease(name.into()));
-        }
+        let name: Arc<str> = name.into();
 
+        if let Some(slot_index) = self.names_to_slot.get(name.as_ref()) {
+            let slot = self.slots.get_mut(*slot_index).unwrap();
+            if let Some(lease) = slot.lease(name, key, self.reserve_time) {
+                return Ok(lease);
+            }
+        }
         Err(NameLeaseError::Taken)
     }
 
@@ -109,7 +122,7 @@ pub enum NameLeaseError {
     Taken,
 }
 
-#[derive(Eq, PartialEq, Clone)]
+#[derive(Eq, PartialEq, Clone, Hash)]
 pub struct Key {
     uuid: Uuid,
     anon: bool,
